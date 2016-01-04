9391ed20a57501cc9c72edd0e373a0193aa8f8b3
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-01-04 11:25:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/telemark/docxtemplater-webservice-docker.svg?branch=master)](https://travis-ci.org/telemark/docxtemplater-webservice-docker)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 # docxtemplater-webservice-docker
 Upload template and data, get formatted document in return
---
+++ handlers/index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+var fs = require('fs')
+var uuid = require('uuid')
+var createDocument = require('../lib/create-document')
+
+function showFrontpage (request, reply) {
+  reply({
+    message: 'Only POST is supported'
+  })
+}
+
+function handleUpload (request, reply) {
+  var convertToFormat = request.params.format
+  var data = request.payload
+  if (data.file) {
+    var nameArray = data.file.hapi.filename.split('.')
+    var newNameConverted = nameArray.join('.') + '.' + convertToFormat
+    var fileEndingOriginal = nameArray.pop()
+    var temporaryName = uuid.v4()
+    var pathPre = process.cwd() + '/uploads/' + temporaryName
+    var fileNameTempOriginal = pathPre + '.' + fileEndingOriginal
+    var fileNameTempConverted = pathPre + '.' + convertToFormat
+    var file = fs.createWriteStream(fileNameTempOriginal)
+
+    file.on('error', function (err) {
+      console.error(err)
+    })
+
+    data.file.pipe(file)
+
+    data.file.on('end', function (err) {
+      if (err) {
+        reply(err)
+      } else {
+        createDocument(fileNameTempOriginal, convertToFormat, function (err, result) {
+          if (err) {
+            reply(err)
+          } else {
+            fs.writeFile(fileNameTempConverted, result, function (err) {
+              if (err) {
+                reply(err)
+              } else {
+                reply.file(fileNameTempConverted, {
+                  filename: newNameConverted
+                }).on('finish', function () {
+                  fs.unlink(fileNameTempOriginal)
+                  fs.unlink(fileNameTempConverted)
+                })
+              }
+            })
+          }
+        })
+      }
+    })
+  }
+}
+
+module.exports.showFrontpage = showFrontpage
+
+module.exports.handleUpload = handleUpload
